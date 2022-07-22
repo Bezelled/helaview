52f54d8b6bd05cfbc548d804a0d9794c3e4b1fd4
Bezelled <cb009351@students.apiit.lk>
2022-07-22 23:12:53 +0530

    Database queries and password validation

--- src/api/users/login.ts
+++ src/api/users/login.ts
@@ -1,7 +1,9 @@
 'use strict';
 
 import { Router, Request, Response } from 'express';
+import { compare } from 'bcrypt';
 import { userLoginKeys } from '../../config/globals.js';
+import hdb from '../../lib/db.js';
 import { validatePostData } from '../../lib/shared.js';
 
 const routes: Router = Router();
@@ -11,6 +13,20 @@ routes.post('/users/login', async (req: Request, res: Response) => {
     
     if (proceed === false)
         return;
+
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+    const hashedPassword = await hdb`
+        SELECT hash FROM tourists WHERE email = ${email};
+    `;
+
+    if (hashedPassword === undefined)
+        return res.status(400).send({ error: `That account does not exist. Please consider registering beforehand.` });
+    
+    const valid: boolean = await compare(password, String(hashedPassword));
+    
+    if (valid === true)
+        console.log(`${email} has successfully logged in.`);
 
     res.send("Welcome to HelaView's API - login!");
 });
--- src/config/globals.ts
+++ src/config/globals.ts
@@ -12,7 +12,7 @@ export const PORT: number = Number(process.env.PORT) || 80;  //Use the port envi
 export const saltRounds: number = 10;
 export const userRegistrationKeys: string[] = ['first name', 'last name', 'email', 'password', 'password confirmation', 'passport number', 'gender', 'age', 'country', 'address', 'contact number'];
 export const hotelRegistrationKeys: string[] = ['full name', 'password', 'email', 'address', 'contact number', 'hotel type', 'rating'];
-export const userLoginKeys: string[] =['email', 'password'];
+export const userLoginKeys: string[] = ['email', 'password'];
 
 //Regular expressions
 
