2c3aea7a67fbd879bf5bcc450e6d5ba17bda121a
Bezelled <cb009351@students.apiit.lk>
2022-07-29 11:38:39 +0530

    Test

--- server/api/hotels/routes.ts
+++ server/api/hotels/routes.ts
@@ -4,13 +4,21 @@ import { Router } from 'express';
 import { readdirSync } from 'fs';
 import { join }  from 'path';
 import { fileURLToPath } from 'url';
+import { platform } from 'os';
 
+console.log(platform());
 const hotelRouter: Router = Router();
 
 async function* getHotelRoutes(){
 
     const __filename: string = fileURLToPath(import.meta.url);
-    const _dirname: string = `file:///${__filename.substring(0, __filename.lastIndexOf('\\routes.js'))}`;
+    let _dirname: string = '';
+    
+    if (platform() === 'linux'){
+        _dirname = `file:///${__filename.substring(0, __filename.lastIndexOf('/routes.js'))}`;
+    } else {
+        _dirname = `file:///${__filename.substring(0, __filename.lastIndexOf('\\routes.js'))}`;
+    };
 
     console.log(`[HOTEL DIRNAME]: ${_dirname}`);
 
