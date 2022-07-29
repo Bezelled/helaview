b755cf93c29f410664f8beccf20b6294c3913306
Bezelled <cb009351@students.apiit.lk>
2022-07-29 11:54:08 +0530

    Tests

--- server/api/tourists/test.js
+++ server/api/tourists/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { readdirSync } from 'fs';
-// import { join }  from 'path';
+import { join }  from 'path';
 import { fileURLToPath } from 'url';
 import { platform } from 'os';
 
@@ -9,21 +9,36 @@ console.log(platform());
 
 const __filename = fileURLToPath(import.meta.url);
 let _dirname = '';
+let _filename = '';
 
-if (platform() === 'linux'){
-    _dirname = __filename.substring(0, __filename.lastIndexOf('/test.js'));
-} else {
-    _dirname = __filename.substring(0, __filename.lastIndexOf('\\test.js'));
-};
-const _filename = `file:///${__filename.substring(0, __filename.lastIndexOf('/test.js'))}`;
-
-console.log(`[DIRNAME]: ${_dirname}`);
-console.log(`[FILENAME]: ${_filename}`);
-
-for (const routeFile of readdirSync(_dirname)) {
+async function* getTests(){
     
-    if ((routeFile === 'routes.js') || (!(routeFile.endsWith('.js'))))
-        continue;
-    
-    console.log(`[Adding hotel route]: ${routeFile}.`);
+    if (platform() === 'linux'){
+        _dirname = __filename.substring(0, __filename.lastIndexOf('/test.js'));
+        _filename = `file:///${__filename.substring(0, __filename.lastIndexOf('/test.js'))}`;
+    } else {
+        _dirname = __filename.substring(0, __filename.lastIndexOf('\\test.js'));
+        _filename = `file:///${__filename.substring(0, __filename.lastIndexOf('\\test.js'))}`;
+    };
+
+    console.log(`[DIRNAME]: ${_dirname}`);
+    console.log(`[FILENAME]: ${_filename}`);
+
+    for (const testFile of readdirSync(_dirname)) {
+        
+        if ((testFile === 'test.js') || (!(testFile.endsWith('.js'))))
+            continue;
+        
+        console.log(`[Adding test]: ${testFile}.`);
+        yield await import(join(_dirname, testFile));
+    }
 }
+
+(async () => {
+    
+    for await (const {default: test} of getTests()) {
+        
+        if (typeof test === 'function')
+            test();
+    };
+})();
