b82ab8e7491f6c62fc85bc72c1b83640b61aa131
Bezelled <83602833+Bezelled@users.noreply.github.com>
2022-08-19 16:29:29 +0530

    Added comments

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer';
 
+// lazy() ensures code splitting - so user will not have to download a large JS file
+
 const Landing = lazy(() => import('./pages/LandingPage'));
 const Layout = lazy(() => import('./containers/Layout'));
 const Login = lazy(() => import('./pages/Login'));
@@ -18,9 +20,11 @@ const NotFound = lazy(() => import('./pages/404'));
 export default function App() {
   return (
     <Router>
+        {/* Helps accessibility by announcing to screen readers */}
       <AccessibleNavigationAnnouncer />
       <Switch>
         <Route exact path="/" component={Landing} />
+        {/* Place new routes over this */}
         <Route path="/login" component={Login} />
         <Route path="/about" component={About} />
         <Route path="/tos" component={ToS} />
@@ -30,8 +34,6 @@ export default function App() {
         <Route path="/register-tourist" component={RegisterTourist} />
         <Route path="/register-hotel" component={RegisterHotel} />
         <Route path="/forgot-password" component={ForgotPassword} />
-
-        {/* Place new routes over this */}
         <Route path="/app" component={Layout} />
         <Route component={NotFound} />
       </Switch>
