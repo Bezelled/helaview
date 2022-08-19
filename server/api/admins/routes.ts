b348c19bee0241d500a690c947cf27782c731725
Bezelled <83602833+Bezelled@users.noreply.github.com>
2022-08-19 16:33:40 +0530

    Added comments

--- server/api/admins/routes.ts
+++ server/api/admins/routes.ts
@@ -7,10 +7,14 @@ import { addRoutes } from '../../lib/shared.js';
 import { authenticateJWT } from '../../middleware/auth.js';
 
 const adminRouter: Router = Router();
+
+//  Implement JWT authorisation for all /admin routes
 adminRouter.use(authenticateJWT(AccountType.Admin));
 
+//  Import all files under the admin folder, so they can add their routes to the router
 (async () => {
     await addRoutes(fileURLToPath(import.meta.url), 'admin', adminRouter);
 })();
 
+//  Export it so our app.ts's Express instance can use this router
 export default adminRouter;
