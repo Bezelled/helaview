6ffb7656fe2a0bd73cf6735c3b57393d47b0c127
Bezelled <83602833+Bezelled@users.noreply.github.com>
2022-09-09 01:30:48 +0530

    Added test misc route

--- server/api/misc/misc.ts
+++ server/api/misc/misc.ts
@@ -1,3 +1,14 @@
 'use strict';
 
-//Place misc API routes here.
+import { Request, Response, Router } from 'express';
+
+//Hardcoded routes
+
+export default async function addRoute(router: Router): Promise<void>{
+
+    router.get('/', (req: Request, res: Response) => {
+        res.sendFile('/client/build/index.html', { root: '.' });
+        // res.send('Welcome to HelaView!');
+    });
+
+}
